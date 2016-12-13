923f59d20f5d16278a7fdcf3668ec67c0b063183
Miroslav Georgiev <miroslav.georgiev@regeneron.com>
2016-12-13 14:48:48 -0500

    updated the startup command

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -9,7 +9,7 @@ cd $DESTINATION_PATH
 # and (re)start forever
 ps ax | grep -v grep | grep forever > /dev/null
 if [ $? != 0 ]; then
-    PORT=$PORT ENV=$ENV SECRET=$SECRET LDAPUSER=$LDAPUSER LDAPPASS=$LDAPPASS LDAPGROUP=$LDAPGROUP APP_DEBUG=$APP_DEBUG forever start -l /webapps/logs/$APPLICATION_NAME.forever.log -o /webapps/logs/$APPLICATION_NAME.out.log -e /webapps/logs/$APPLICATION_NAME.err.log -a --max-old-space-size=$MAX_V8_HEAP_SIZE cluster.js
+    PORT=$PORT ENV=$ENV SECRET=$SECRET LDAPUSER=$LDAPUSER LDAPPASS=$LDAPPASS LDAPGROUP=$LDAPGROUP APP_DEBUG=$APP_DEBUG forever start -c "node --max-old-space-size=$MAX_V8_HEAP_SIZE --nouse-idle-notification" -l /webapps/logs/$APPLICATION_NAME.forever.log -o /webapps/logs/$APPLICATION_NAME.out.log -e /webapps/logs/$APPLICATION_NAME.err.log -a cluster.js
 else
     forever restart cluster.js
 fi
